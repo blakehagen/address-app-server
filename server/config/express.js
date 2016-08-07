dfb43aa1b79895dc14528a97b149f8c028a844b4
Blake Hagen <blakehagen@gmail.com>
2016-08-07 16:07:16 -0600

    cors

--- server/config/express.js
+++ server/config/express.js
@@ -10,6 +10,7 @@ const logger     = require('morgan');
 module.exports = () => {
   const app = express();
 
+
   app.use(logger('dev'));
   app.use(cors());
   app.use(bodyParser.json());
@@ -19,6 +20,13 @@ module.exports = () => {
     resave: false,
     saveUninitialized: true
   }));
+
+  app.all('/*', function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', req.headers.origin || '*');
+    res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,HEAD,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'content-Type,x-requested-with');
+    next();
+  });
 
   // app.use(express.static('www'));
   return app;
