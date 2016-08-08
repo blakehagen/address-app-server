3ad7b84a99c431a1b862b97529dd82f98f0d6b4b
Blake Hagen <blakehagen@gmail.com>
2016-08-07 19:03:21 -0600

    again

--- server/features/user/user.server.controller.js
+++ server/features/user/user.server.controller.js
@@ -3,9 +3,6 @@
 module.exports = {
 
   signupSuccess: (req, res) => {
-    console.log('req:::::::::::::::::::::::::::::::: ', req);
-    console.log('req.body:::::::::::::::::::::::::::::::: ', req.body);
-
     res.status(200).json({user: req.user, message: 'Success'});
   },
 
