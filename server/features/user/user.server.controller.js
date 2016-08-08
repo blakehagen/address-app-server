9120ee9cf2642dc2fd32349f4d4b7430f44d8144
Blake Hagen <blakehagen@gmail.com>
2016-08-07 18:54:43 -0600

    modifying auth routes

--- server/features/user/user.server.controller.js
+++ server/features/user/user.server.controller.js
@@ -3,6 +3,9 @@
 module.exports = {
 
   signupSuccess: (req, res) => {
+    console.log('req:::::::::::::::::::::::::::::::: ', req);
+    console.log('req.body:::::::::::::::::::::::::::::::: ', req.body);
+
     res.status(200).json({user: req.user, message: 'Success'});
   },
 
