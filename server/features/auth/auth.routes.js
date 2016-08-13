d0150d50a26e4922cebbaab4fdf2bc83cc87a1b2
Blake Hagen <blakehagen@gmail.com>
2016-08-13 11:49:56 -0600

    adding who is user route

--- server/features/auth/auth.routes.js
+++ server/features/auth/auth.routes.js
@@ -40,4 +40,23 @@ module.exports = (app, passport) => {
     res.status(200).json({user: req.user, message: 'Success'});
   });
 
+  //====================//
+  // USER LOGOUT ROUTES //
+  //===================//
+  app.get('/logout', function(req, res){
+    req.logout();
+    res.redirect('/');
+  });
+
+  //===============//
+  // WHO IS USER? //
+  //==============//
+  app.get('/api/v1/me', function(req, res){
+    if(req.user){
+      res.status(200).json(req.user)
+    } else {
+      res.status(401).send('NO CURRENT USER');
+    }
+  });
+
 };
